d48106f1025794a4fe32a26da4c54aa2bac2eb39
KKS161994 <kartikeyshrivastava@gmail.com>
2019-02-14 00:45:12 +0530

    gradle issues resolved

--- app/src/main/java/croom/konekom/in/learningkotlin/MainActivity.kt
+++ app/src/main/java/croom/konekom/in/learningkotlin/MainActivity.kt
@@ -1,10 +1,13 @@
 package croom.konekom.`in`.learningkotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by kartikey on 13/02/19.
+ */
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
